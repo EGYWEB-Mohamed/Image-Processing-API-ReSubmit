82a23e2b9dc9f28dede65d4a96691cb253171ef4
Mohamed Said <ceo@egyweb.info>
2022-03-14 15:01:26 +0200

    Fix Validations
    Check if Int or not

--- src/utilities/Validations.ts
+++ src/utilities/Validations.ts
@@ -9,11 +9,20 @@ export const ResizeQueryValidator = async (
   if (!params.width || !params.height || !params.source) {
     return "Parameter {width & height & source} Cant Be Empty";
   }
-  if (validator.isInt(params.width) && parseInt(params.width) <= 100) {
-    return "Width Should be Integer And More Then 100px";
+
+  if (!validator.isInt(params.width)) {
+    return "Width Should be Integer";
   }
-  if (validator.isInt(params.height) && parseInt(params.height) <= 100) {
-    return "Height Should be Integer And More Then 100px";
+  if (parseInt(params.width) <= 100) {
+    return "Width Should More Then 100px";
+  }
+
+  if (!validator.isInt(params.height)) {
+    return "Height Should be Integer";
+  }
+
+  if (parseInt(params.height) <= 100) {
+    return "Height Should More Then 100px";
   }
   if (!fs.existsSync(Paths.srcImagePath + "/" + params.source)) {
     return "Image Selected Is Not Found in Source Directory";
